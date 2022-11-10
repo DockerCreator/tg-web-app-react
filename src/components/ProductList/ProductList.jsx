c467c12380d1b8e762a90b8536509e77c21f06f6
DockerCreator <virusenek@gmail.com>
2022-11-10 06:24:35 +0300

    upd18

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { useTelegram } from '../../hooks/useTelegram'
 import ProductItem from '../ProductItem/ProductItem'
 import './ProductList.css'
@@ -23,6 +23,28 @@ const getTotalPrice = (items = []) => {
 const ProductList = () => {
 	const [addedItems, setAddedItems] = useState([])
 	const { tg } = useTelegram()
+
+	const onSendData = useCallback(() => {
+		const data = {
+			products: addedItems,
+			totalPrice: getTotalPrice(addedItems),
+		}
+		fetch('https://localhost:8000', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		})
+	}, [])
+
+	useEffect(() => {
+		tg.onEvent('mainButtonClicked', onSendData)
+		return () => {
+			tg.offEvent('mainButtonClicked', onSendData)
+		}
+	}, [onSendData])
+
 	const onAdd = product => {
 		const alreadyAdded = addedItems.find(item => item.id === product.id)
 		let newItems = []
